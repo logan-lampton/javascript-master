9a0cb0ecf0ec591917f84ed5e06e8f1eb125dd00
Logan Lampton <lampton.logan@gmail.com>
2023-02-04 16:49:32 -0500

    data types assignment completed

--- fundamentals/assignments.js
+++ fundamentals/assignments.js
@@ -4,3 +4,14 @@ const continent = "North America";
 let population = 332000000;
 
 console.log(country, continent, population);
+
+// Data Types
+let isIsland = false;
+let language;
+
+console.log(
+    typeof isIsland, 
+    typeof population,
+    typeof country,
+    typeof language
+);
--- fundamentals/fundamentals-a.js
+++ fundamentals/fundamentals-a.js
@@ -1,6 +1,20 @@
+// Values and Variables
 //a value is the smallest unit in JS
 // 4 + 56 + 100 are three values that are combined by mathmatical operators to make one value
 // 'Logan' is a value
 
 let js = "amazing";
 // js is a variable assigned to the value 'amazing'
+
+// Data Types
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined (not yet defined)
+// 5. Null (it IS NOT an object)
+// 6. Symbol
+// 7. BigInt (larger integers than the number type can hold)
+
+// JS has dynamic typing - you do NOT have to manually define the data type; value has type, not the variable
+// console.log(typeof 456) returns number
+// if you don't assign a value to a variable, it will have the type of undefined
