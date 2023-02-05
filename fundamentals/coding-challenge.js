a84e29154a2e2cc370c5db4f4cda29b9be7cefb9
Logan Lampton <lampton.logan@gmail.com>
2023-02-05 12:21:39 -0500

    completed code challenge 3

--- fundamentals/coding-challenge.js
+++ fundamentals/coding-challenge.js
@@ -18,12 +18,41 @@ const markHigherBMI = markBMI > johnBMI;
 // Coding Challenge #2
 // My solution
 if (markBMI > johnBMI) {
-    console.log(`Mark's BMI (${markBMI}) is ${markBMI - johnBMI} higher than John's BMI (${johnBMI}).`);
+  console.log(
+    `Mark's BMI (${markBMI}) is ${
+      markBMI - johnBMI
+    } higher than John's BMI (${johnBMI}).`
+  );
 } else if (johnBMI > markBMI) {
-    console.log(`Johns's BMI (${johnBMI}) is ${johnBMI - markBMI} higher than Marks's BMI (${markBMI}).`);
+  console.log(
+    `Johns's BMI (${johnBMI}) is ${
+      johnBMI - markBMI
+    } higher than Marks's BMI (${markBMI}).`
+  );
 } else {
-    console.log(`Mark and John have the same BMI (${markBMI})!`)
-};
+  console.log(`Mark and John have the same BMI (${markBMI})!`);
+}
 // Instructor solution was basically the same, just not as descriptive
 
 // Coding Challenge #3
+const dolphinsAverageScore = (96 + 108 + 89) / 3;
+const koalasAverageScore = (88 + 91 + 110) / 3;
+if (dolphinsAverageScore > koalasAverageScore) {
+  console.log(`Dolphins won with an average score of ${dolphinsAverageScore}!`);
+} else if (koalasAverageScore > dolphinsAverageScore) {
+  console.log(`Koalas won with an average score of ${koalasAverageScore}!`);
+} else {
+  console.log(`It was a tie, with an average score of ${dolphinsAverageScore}`);
+};
+// Bonus 1 mixed with Bonus 2
+const dolphinsBonusOne = (97 + 112 + 101) / 3;
+const koalasBonusOne = (109 + 95 + 106) / 3;
+if (dolphinsBonusOne > koalasBonusOne && dolphinsBonusOne >= 100) {
+    console.log(`Dolphins won with an average score of ${dolphinsBonusOne}!`);
+  } else if (koalasBonusOne > dolphinsBonusOne && koalasBonusOne >= 100) {
+    console.log(`Koalas won with an average score of ${koalasBonusOne}!`);
+  } else if (koalasBonusOne === dolphinsBonusOne && koalasBonusOne >= 100 && dolphinsBonusOne >= 100) {
+    console.log("Wow, it's a tie!")
+  } else {
+    console.log("BOTH LOSE!!!");
+  };
