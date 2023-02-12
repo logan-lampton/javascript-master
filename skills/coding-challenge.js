55e2b11acb3c0cc9c7baf0f3e4b05138916cfcef
Logan Lampton <lampton.logan@gmail.com>
2023-02-12 16:30:38 -0500

    refined solution to coding challenge

--- skills/coding-challenge.js
+++ skills/coding-challenge.js
@@ -1,9 +1,12 @@
 // Given an array of forecasted maximum temperatures the thermometer displays a string. Create a function 'printForecast' which takes in an array 'arr' and logs a string to the console.
 
 function printForecast(arr) {
+    let str = "";
     for (i = 0; i < arr.length; i++) {
-        console.log(`... ${arr[i]} in ${i + 1} days`);
+        //str += is the same as str = str + etc...
+        str += `${arr[i]}°C in ${i + 1} days ... `;
     }
+    console.log("... " + str);
 };
 
 const array1 = [17, 21, 23];
