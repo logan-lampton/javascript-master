def9a814131affa6220d753f68805d847dafd7d3
Logan Lampton <lampton.logan@gmail.com>
2023-02-13 00:04:53 -0500

    completed lesson on hoisting

--- section-8/script.js
+++ section-8/script.js
@@ -9,11 +9,11 @@ function calcAge(birthYear) {
   const age = year - birthYear;
 
   function printAge() {
-    const output = `${firstName}, you are ${age} years old. You were born in ${birthYear}.`
+    const output = `${firstName}, you are ${age} years old. You were born in ${birthYear}.`;
     console.log(output);
 
-    if(birthYear >= 1981 && birthYear <= 1996) {
-      const string = `You're a Millenial, ${firstName}!`
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const string = `You're a Millenial, ${firstName}!`;
       console.log(string);
 
       function add(a, b) {
@@ -21,7 +21,6 @@ function calcAge(birthYear) {
       }
       add(2, 3);
     }
-    
   }
   printAge();
   return age;
@@ -30,6 +29,39 @@ console.log(calcAge(1988));
 
 ///////////////////////////////////////
 // Hoisting and TDZ in Practice
+// console.log(me);
+// console.log(job);
+// console.log(birthYear);
+
+// can't hoist let or const (var returns undefined)
+var me = 'Logan';
+let job = 'software engineer';
+const birthYear = 1988;
+
+console.log(addDecl(2, 3));
+// console.log(addArrow(2,3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+// really shouldn't have the if statement first, but with a var, it will return undefined and assume that there are no products
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+// can't hoist arrow functions
+// const addArrow = (a, b) => a + b;
+
+// variables declared with let or const don't appear as properties of the global window object of the browser
+var x = 1;
+let y = 2;
+const z = 3;
 
 ///////////////////////////////////////
 // The this Keyword in Practice
