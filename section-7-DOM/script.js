fd0f0e56b45dd554780d574cd14ee05363be2de9
Logan Lampton <lampton.logan@gmail.com>
2023-02-17 14:58:53 -0500

    click events

--- section-7-DOM/script.js
+++ section-7-DOM/script.js
@@ -9,8 +9,22 @@
 // DOM manipulation is part of web APIs that JS can manipulate
 // no need to import anything
 
-document.querySelector('.message').textContent = "Correct Number 🥳"
+// document.querySelector('.message').textContent = "Correct Number 🥳"
 
-document.querySelector(".number").textContent = 13;
+// document.querySelector(".number").textContent = 13;
 
-document.querySelector(".guess").value = 19;
+// document.querySelector(".guess").value = 19;
+
+// The function is not called immediately, ONLY when the click occurs
+document.querySelector('.check').addEventListener('click', handleClick);
+
+function handleClick() {
+  // the input is a string, so need to convert to a number
+  const guess = Number(document.querySelector('.guess').value);
+
+  //  first scenario with DOM manipulation is to handle if there is no input provided
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'No number provided; please guess a number!';
+  }
+}
