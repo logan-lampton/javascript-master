43b6107cc382cc00aa34af19becd08ee7ea724a7
Logan Lampton <lampton.logan@gmail.com>
2023-02-17 14:39:55 -0500

    lesson on DOM manipulation

--- section-7-DOM/script.js
+++ section-7-DOM/script.js
@@ -1,4 +1,16 @@
 'use strict';
 
-document.querySelector('.message');
-console.log(document.querySelector('.message').textContent);
+// document is the entry point to the DOM
+
+// DOM tree
+// document => HTML => head and body => titles, sections, etc. => p, links, etc. => etc.
+
+// DOM manipulation is not techincally part of JS
+// DOM manipulation is part of web APIs that JS can manipulate
+// no need to import anything
+
+document.querySelector('.message').textContent = "Correct Number 🥳"
+
+document.querySelector(".number").textContent = 13;
+
+document.querySelector(".guess").value = 19;
