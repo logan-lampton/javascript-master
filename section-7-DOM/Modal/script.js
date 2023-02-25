44c42b6b4c6d99fc569c17c7bdba78ce1f32c0e7
Logan Lampton <lampton.logan@gmail.com>
2023-02-24 23:00:09 -0500

    event listeners to close and open modal

--- section-7-DOM/Modal/script.js
+++ section-7-DOM/Modal/script.js
@@ -6,6 +6,24 @@ const btnCloseModal = document.querySelector('.close-modal');
 // querySelector would only select the first .show-modal; querySelectorAll selects them all
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+// "opening" the modal
 for (let i = 0; i < btnsOpenModal.length; i++) {
-    console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', function () {
+    // ".hidden" is only for the selector
+    modal.classList.remove('hidden');
+    // the overlay is a blur in the CSS hidden in the HTML on load
+    overlay.classList.remove('hidden');
+  });
 }
+
+// "closing" the modal function
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// "closing" the modal
+modal.addEventListener('click', closeModal);
+
+// "closing" the modal by clicking anywhere in the overlay
+overlay.addEventListener('click', closeModal);
