453bef5361d06263d49d7e410b49f1094d971b8d
Logan Lampton <lampton.logan@gmail.com>
2023-02-25 17:07:01 -0500

    demonstated old way to establish default values

--- section-10-functions/script.js
+++ section-10-functions/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// This is the OLD way to assign default parameters
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers, price) {
+  const booking = {
+    // flightNum is the same as flightNum: flightNum
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("Test");
+// console.log(booking);
