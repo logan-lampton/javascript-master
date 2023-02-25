718f9ecbed9d1008e986332493de309b6ac01851
Logan Lampton <lampton.logan@gmail.com>
2023-02-25 00:20:50 -0500

    added logic for resetting current score and toggling which player is active

--- section-7-DOM/Pig Game/script.js
+++ section-7-DOM/Pig Game/script.js
@@ -5,8 +5,11 @@
 const playerOneScoreEl = document.querySelector('#score--0');
 const playerTwoScoreEl = document.getElementById('score--1');
 // player current score elements
-const playerOneCurrentEl = document.getElementById("current--0")
-const playerTwoCurrentEl = document.getElementById("current--1")
+const playerOneCurrentEl = document.getElementById('current--0');
+const playerTwoCurrentEl = document.getElementById('current--1');
+// Overall player elements, for toggling which is active
+const playerOneEl = document.querySelector('.player--0');
+const playerTwoEl = document.querySelector('.player--1');
 // select the dice element
 const diceEl = document.querySelector('.dice');
 // selecting the buttons
@@ -25,6 +28,11 @@ diceEl.classList.add('hidden');
 // Needs to be let as we will keep updating it
 // Needs to be outside of the rollBtn click event so that it isn't reset each roll
 let currentScore = 0;
+// set the active player to switch
+let activePlayer = 0;
+// scores array; will be updated in this array
+// player 1 at position 0, player 2 at position 1
+const scores = [0, 0];
 
 rollBtn.addEventListener('click', function () {
   // Rolling the dice; generating a number between 1-6
@@ -37,10 +45,17 @@ rollBtn.addEventListener('click', function () {
   if (dice !== 1) {
     // this is shorthand for currentScore = currentScore + dice
     currentScore += dice;
-    playerOneCurrentEl.textContent = currentScore
-    // WILL NEED TO UPDATE THE LINE ABOVE
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    // set currentScore back to 0
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     // Switch to the other player's turn
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    playerOneEl.classList.toggle('player--active');
+    playerTwoEl.classList.toggle('player--active');
   }
   console.log(dice);
 });
