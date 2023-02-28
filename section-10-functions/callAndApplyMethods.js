160687ae88663802249a198b1ccf0142a6d8b0c6
Logan Lampton <lampton.logan@gmail.com>
2023-02-28 15:36:34 -0500

    wrapped lesson on call and apply methods

--- section-10-functions/callAndApplyMethods.js
+++ section-10-functions/callAndApplyMethods.js
@@ -33,6 +33,7 @@ const eurowings = {
 // Since book is a copy and now being called as a regular function, rather than a method within the lufhansa object. Therefore, the this keyword will point to UNDEFINED and it will throw an error
 // book(1337, "Chuck Norris")
 
+// Call Method
 // the call method allows us to use methods/functions with the this keyword
 // the first argument of the call method is the location you want the function to run, in this case the eurowings object. We then add the other arguements the function needs to run
 book.call(eurowings, 23, 'Sara Williams');
@@ -40,3 +41,10 @@ book.call(lufthansa, 418, 'Big Mac');
 book.call(eurowings, 623, 'Niko Bellic');
 console.log(eurowings.bookings);
 console.log(lufthansa.bookings);
+
+// Apply Method
+const flightData = [583, 'George Carlin'];
+// first argument is the function to use, the second is the variable to be "applied"
+book.apply(eurowings, flightData);
+// The applied method is not used very often anymore; Now we use the spread operator
+book.call(eurowings, ...flightData);
