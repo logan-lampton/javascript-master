32365f54ffd9cc37c633af0b3a01f512e5037a60
Logan Lampton <lampton.logan@gmail.com>
2023-03-15 15:01:49 -0400

    added timer to loan approval in Bankist app

--- section-12-numbers/Bankist/script.js
+++ section-12-numbers/Bankist/script.js
@@ -291,15 +291,17 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add transfer date
-    const currentDate = new Date().toISOString();
-    currentAccount.movementsDates.push(currentDate);
+      // Add transfer date
+      const currentDate = new Date().toISOString();
+      currentAccount.movementsDates.push(currentDate);
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = '';
 });
