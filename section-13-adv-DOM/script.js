324ad2b751feffe162010d885f1c4dd4b2721038
Logan Lampton <lampton.logan@gmail.com>
2023-03-21 14:56:54 -0400

    added lazy image loading

--- section-13-adv-DOM/script.js
+++ section-13-adv-DOM/script.js
@@ -195,24 +195,47 @@ headerObserver.observe(header);
 ///////////////////////////////////////
 // Revealing Sections on Scroll
 
-const allSections = document.querySelectorAll(".section")
+const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  if(!entry.isIntersecting) return;
-  entry.target.classList.remove("section--hidden");
-  observer.unobserve(entry.target)
-}
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
 
-const sectionObserver = new IntersectionObserver(revealSection, { 
+const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
   threshold: 0.15,
-})
+});
 
-allSections.forEach(function(section) {
+allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add("section--hidden");
-})
+  section.classList.add('section--hidden');
+});
+
+///////////////////////////////////////
+// Lazy Loading Images
+// This is very good for performance
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, observer) {
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  // replace src attribute with the data-src attribute
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  })
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px"
+});
+imgTargets.forEach(img => imgObserver.observe(img));
 
 ///////////////////////////////////////
 // DOM Traversing
