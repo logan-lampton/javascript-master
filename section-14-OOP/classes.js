a14be19714a7d8e2b6efc4fa36bcb1008385aa1d
Logan Lampton <lampton.logan@gmail.com>
2023-03-23 16:27:20 -0400

    lesson on getters and setters

--- section-14-OOP/classes.js
+++ section-14-OOP/classes.js
@@ -8,8 +8,8 @@
 // class declaration
 class PersonCl {
   // the constructor is a method of the class
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -18,11 +18,31 @@ class PersonCl {
   }
 
   greet() {
-    console.log(`Hey, ${this.firstName}!`);
+    console.log(`Hey, ${this.fullName}!`);
+  }
+
+  get age() {
+    return new Date().getFullYear() - this.birthYear;
+  }
+
+  // test to see if the fullName provided has a first and last name
+  // this._ is just a naming convention, not a rule
+  // we are creating a new fullName variable with this._
+  set fullName(name) {
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name (needs a first and last name)`);
+    }
+  }
+  //   the getter function allows the setter variable to update the original variable
+  // Otherwise fullName is undefined
+  get fullName() {
+    return this._fullName;
   }
 }
 
-const jessica = new PersonCl('Jessica', 1989);
+const jessica = new PersonCl('Jessica Landrey', 1989);
 console.log(jessica);
 jessica.calcAge();
 
@@ -34,7 +54,39 @@ jessica.calcAge();
 // };
 jessica.greet();
 
+console.log(jessica.age);
+console.log(jessica.fullName);
+
 // classes are NOT hoisted (unlike function declarations)
 // classes are first-class citizens; meaning, they can be passed into functions and also be returned from functions, because classes are just a special type of function
 // the body of a class is ALWAYS executed in strict mode
 
+// /////////////////////////////////////////////////
+// Getters and Setters
+
+// functions that get and set a value, but still look like regular properties
+
+const account = {
+  owner: 'jonas',
+  movements: [200, 530, 120, 300],
+
+  // to turn into a getter, simply prepend "get"
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  // each setter must have exactly one argument
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+// setters are called like any other property
+account.latest = 600;
+console.log(account.movements);
+
+const walter = new PersonCl('Walter Cronchite', 1965);
+console.log(walter);
+
+// You don't have to use getters and setters, but they can help a lot with validations
